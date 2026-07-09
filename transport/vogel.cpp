6f3e11caba7b525b92828084db5b9a68b57fe270
Sterbenxy13 <pauloalvino.moura@gmail.com>
2026-07-09 11:25:54 -0300

    vogel concluído?

--- transport/vogel.cpp
+++ transport/vogel.cpp
@@ -1,6 +1,88 @@
 
 #include "common.h"
 
+Tuple blockLine(AllInOneBox problem, int supplyCount, int demandCount) {
+    int maxSupplyWeight {-999};
+    int minSupplyCost {999};
+    int secMinSupplyCost {999};
+    Tuple minPos = Tuple();
+    for (int s = 0; s < supplyCount; s++) {
+        if (problem.supplies[s].capacity <= 0) {
+            continue;
+        }
+        for (int d = 0; d < demandCount; d++) {
+            if (problem.demands[d].demand <= 0 | problem.matrix[s][d].value < 0) {
+                continue;
+            }
+            if (problem.matrix[s][d].cost < minSupplyCost) {
+                secMinSupplyCost = minSupplyCost;
+                minSupplyCost = problem.matrix[s][d].cost;
+                minPos.dIndex = d;
+            }
+            if (problem.matrix[s][d].cost < secMinSupplyCost && problem.matrix[s][d].cost > minSupplyCost) {
+                secMinSupplyCost = problem.matrix[s][d].cost;
+            }
+            std::cout << "vendo as linhas, olhei para a pos: " << s << " , " << d << std::endl;
+        }
+        if ((secMinSupplyCost - minSupplyCost) > maxSupplyWeight) {
+            minPos.sIndex = s;
+            maxSupplyWeight = secMinSupplyCost - minSupplyCost;
+        }
+    }
+    minSupplyCost = 999;
+    for (int d = 0; d < demandCount; d++) {
+        if (problem.demands[d].demand <= 0 | problem.matrix[minPos.sIndex][d].value < 0) {
+            continue;
+        }
+        if (problem.matrix[minPos.sIndex][d].cost < minSupplyCost) {
+            minSupplyCost = problem.matrix[minPos.sIndex][d].cost;
+            minPos.dIndex = d;
+        }
+    }
+    return minPos;
+}
+
+Tuple blockColumn(AllInOneBox problem, int supplyCount, int demandCount) {
+    int maxDemandWeight {-999};
+    int minDemandCost {999};
+    int secMinDemandCost {999};
+    Tuple minPos = Tuple();
+    for (int d = 0; d < demandCount; d++) {
+        if (problem.demands[d].demand <= 0) {
+            continue;
+        }
+        for (int s = 0; s < supplyCount; s++) {
+            if (problem.supplies[s].capacity <= 0 | problem.matrix[s][d].value < 0) {
+                continue;
+            }
+            if (problem.matrix[s][d].cost < minDemandCost) {
+                secMinDemandCost = minDemandCost;
+                minDemandCost = problem.matrix[s][d].cost;
+                minPos.sIndex = s;
+            }
+            if (problem.matrix[s][d].cost < secMinDemandCost && problem.matrix[s][d].cost > minDemandCost) {
+                secMinDemandCost = problem.matrix[s][d].cost;
+            }
+            std::cout << "vendo as colunas, olhei para a pos: " << s << " , " << d << std::endl;
+        }
+        if ((secMinDemandCost - minDemandCost) > maxDemandWeight) {
+            minPos.dIndex = d;
+            maxDemandWeight = secMinDemandCost - minDemandCost;
+        }
+    }
+    minDemandCost = 999;
+    for (int s = 0; s < supplyCount; s++) {
+        if (problem.supplies[s].capacity <= 0 | problem.matrix[s][minPos.dIndex].value < 0) {
+            continue;
+        }
+        if (problem.matrix[s][minPos.dIndex].cost < minDemandCost) {
+            minDemandCost = problem.matrix[s][minPos.dIndex].cost;
+            minPos.sIndex = s;
+        }
+    }
+    return minPos;
+}
+
 AllInOneBox vogel(AllInOneBox problem, int supplyCount, int demandCount) {
     Memory* memory = problem.initMemory(supplyCount, demandCount);
 
@@ -19,66 +101,29 @@ AllInOneBox vogel(AllInOneBox problem, int supplyCount, int demandCount) {
     Demand* currentDemand;
     Supply* currentSupply;
 
-    int maxSupplyWeight;
-    int minSupplyValue;
-    int secMinSupplyValue;
-
-    int maxDemandWeight;
-    int minDemandValue;
-    int secMinDemandValue;
-
     Tuple minPos;
     minPos.sIndex;
     minPos.dIndex;
 
     std::cout << "vai iniciar o while" << std::endl;
+
+    bool linha = true;
+
+    int nada = 0;
     while (remainingDemand > 0) {
-        minSupplyValue = 999;
-        secMinSupplyValue = 999;
-        minPos.sIndex = 0;
-        minPos.dIndex = 0;
+        // minSupplyValue = 999;
+        // secMinSupplyValue = 999;
+        // minPos.sIndex = 0;
+        // minPos.dIndex = 0;
 
-        for (int i = 0; i < supplyCount; i++) {
-            if (supplies[i].capacity <= 0) {
-                continue;
-            }
-            for (int j = 0; j < demandCount; j++) {
-                if (demands[i].demand <= 0) {
-                    continue;
-                }
-                if (problem.matrix[i][j].cost < minSupplyValue) {
-                    secMinSupplyValue = minSupplyValue;
-                    minSupplyValue = problem.matrix[i][j].cost;
-                }
-            }
-            if ((secMinSupplyValue - minSupplyValue) > maxSupplyWeight) {
-                minPos.sIndex = i;
-                maxSupplyWeight = secMinSupplyValue - minSupplyValue;
-            }
+        if (linha) {
+            minPos = blockLine(problem, supplyCount, demandCount);
+        } else {
+            minPos = blockColumn(problem, supplyCount, demandCount);
         }
+        linha = ! linha;
 
-        std::cout << "definiu oferta" << std::endl;
-
-        for (int i = 0; i < demandCount; i++) {
-            if (demands[i].demand <= 0) {
-                continue;
-            }
-            for (int j = 0; j < supplyCount; j++) {
-                if (supplies[i].capacity <= 0) {
-                    continue;
-                }
-                if (problem.matrix[j][i].cost < minDemandValue) {
-                    secMinDemandValue = minDemandValue;
-                    minDemandValue = problem.matrix[j][i].cost;
-                }
-                std::cout << "verificando (d,s): " << i << " , " << j << std::endl;
-            }
-            if ((secMinDemandValue - minDemandValue) > maxDemandWeight) {
-                minPos.dIndex = i;
-            }
-        }
-
-        std::cout << "definiu demanda" << std::endl;
+        std::cout << "definiu oferta e demanda: " << minPos.sIndex << " , " << minPos.dIndex << std::endl;
 
         currentDemand = &(demands[minPos.dIndex]);
         currentSupply = &(supplies[minPos.sIndex]);
@@ -87,17 +132,18 @@ AllInOneBox vogel(AllInOneBox problem, int supplyCount, int demandCount) {
 
         currentCapacity = (*currentSupply).capacity;
 
-        std::cout << "pedou capacidade atual" << std::endl;
+        std::cout << "pegou capacidade atual" << std::endl;
 
         currentDemandValue = (*currentDemand).demand;
 
         std::cout << "pegou demanda atual" << std::endl;
 
-        std::cout << "endereco da memoria: " << &memory << std::endl;
-        std::cout << "tamanho da memoria: " << memory->size << std::endl;
-        std::cout << "ultimo da memoria: " << memory->getLast().sIndex << " , " << memory->getLast().dIndex << std::endl;
-
+        // std::cout << "endereco da memoria: " << &(memory->getLast()) << std::endl;
+        // std::cout << "ultimo da memoria: " << memory->getLast().sIndex << " , " << memory->getLast().dIndex << std::endl;
+        
         (*memory).append(minPos.sIndex, minPos.dIndex);
+
+        std::cout << "tamanho da memoria: " << memory->size << std::endl;
 
         std::cout << "appendeu memoria" << std::endl;
 
@@ -130,10 +176,12 @@ AllInOneBox vogel(AllInOneBox problem, int supplyCount, int demandCount) {
 
         std::cout << "resolveu ifs" << std::endl;
 
-        (*currentSupply).capacity = currentCapacity - currentReduction;
-        (*currentDemand).demand = currentDemandValue - currentReduction;
+        (*currentSupply).capacity = (*currentSupply).capacity - currentReduction;
+        (*currentDemand).demand = (*currentDemand).demand - currentReduction;
         remainingDemand = remainingDemand - currentReduction;
         problem.matrix[minPos.sIndex][minPos.dIndex].value = currentReduction;
+
+        // std::cin >> nada;
     }
     return problem;
 }
